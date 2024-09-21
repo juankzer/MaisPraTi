ee3a4f067d627bf92ec08f4e0e4e19abd0da37ab
juankzer <juanalexandrectt@gmail.com>
2024-09-20 21:32:13 -0300

    update

--- ex1.js
+++ ex1.js
@@ -1,5 +1,5 @@
 //Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
-//utilizando uma estrutura de controle if.
+//utilizando uma estrutura de controle if comtest.
 
 let num1 = 2
 if (num1 % 2 === 0){
